b223d985abfad960205becda542fbe4b96844a06
W3bReactor <vaironfop@Mail.ru>
2026-07-03 07:03:43 +0300

    Фикс cookie

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,12 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import setCookieParser from "set-cookie-parser";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_HOST
-
-const isProd = process.env.NODE_ENV === 'production';
-const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN;
-
+const BACKEND_URL = process.env.NEXT_PUBLIC_HOST;
 
 export async function POST() {
     const cookieStore = await cookies();
@@ -29,26 +24,18 @@ export async function POST() {
         );
     }
 
-    const setCookies =
-        backendResponse.headers
-            .getSetCookie();
-
-    const parsed = setCookieParser.parse(setCookies, { map: false });
-
-    for (const c of parsed) {
-        (await cookies()).set(c.name, c.value, {
-            httpOnly: true,
-            path: c.path ?? "/",
-            secure: isProd, // ОБЯЗАТЕЛЬНО
-            sameSite: isProd ? 'none' : 'lax', // ОБЯЗАТЕЛЬНО
-            domain: COOKIE_DOMAIN,
-            maxAge: c.maxAge ?? 10
-        });
-    }
-
-
-
-    return NextResponse.json({
+    const response = NextResponse.json({
         success: true
     });
+
+    backendResponse.headers
+        .getSetCookie()
+        .forEach(cookie => {
+            response.headers.append(
+                "Set-Cookie",
+                cookie
+            );
+        });
+
+    return response;
 }
