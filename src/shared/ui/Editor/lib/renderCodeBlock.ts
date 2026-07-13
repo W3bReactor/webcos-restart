dc32a889d43c225da6a7cf6c0c9d38f06e886918
W3bReactor <vaironfop@Mail.ru>
2026-07-14 01:28:00 +0300

    Исправлен renderer и стили

--- src/shared/ui/Editor/lib/renderCodeBlock.ts
+++ src/shared/ui/Editor/lib/renderCodeBlock.ts
@@ -4,22 +4,19 @@ import type { NodeProps } from '@tiptap/static-renderer'
 import type { Node as PMNode } from '@tiptap/pm/model'
 
 export function renderCodeBlock(
-    {node, children}: NodeProps<PMNode, string | string[]>,
+    {node}: NodeProps<PMNode, string | string[]>,
 ): string {
-
     const language =
         typeof node.attrs.language === 'string'
             ? node.attrs.language
             : 'plaintext'
 
-    const code =
-        Array.isArray(children)
-            ? children.join('')
-            : (children ?? '')
+    const code = node.textContent;
 
     const tree = lowlight.registered(language)
         ? lowlight.highlight(language, code)
         : lowlight.highlight('plaintext', code)
+    console.log("Code: " + code)
 
     return `<pre class="article-code"><code class="language-${language}">${toHtml(tree)}</code></pre>`;
 }
